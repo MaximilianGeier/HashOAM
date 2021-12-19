0b97e709bab48c94113032f160745d475a37ffd5
IlyaIIS <ilya1221221@mail.ru>
2021-12-20 00:42:32 +0500

    Добавлена первая шеш коллекция

---
+++ Hash2/ChainHashCollection.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash2
+{
+    class ChainHashCollection<T>
+    {
+        List<T>[] elements;
+        public readonly int Size;
+        public int Count = 0;
+
+        public ChainHashCollection(int size)
+        {
+            Size = size;
+            elements = new List<T>[size];
+        }
+
+        int GetHash(T item)
+        {
+            double key = 0;
+            if (typeof(T) == typeof(double) || typeof(T) == typeof(float) || typeof(T) == typeof(int))
+                key = (double)(object)item;
+            else if (typeof(T) == typeof(string))
+                foreach (char el in item as string)
+                    key += el;
+            else
+                key = item.GetHashCode();
+
+            /*if (key >= 1)
+                return (int)(key % Size);
+            else
+                return (int)(key*Size);*/
+            return (int)(key*Size % Size);
+        }
+
+        public void Add(T item)
+        {
+            int hash = GetHash(item);
+            if (elements[hash] == null)
+                elements[hash] = new List<T> { item };
+            else
+                elements[hash].Add(item);
+
+            Count++;
+        }
+        public bool Remove(T item)
+        {
+            int hash = GetHash(item);
+
+            if (elements[hash] == null)
+                return false;
+            else
+                for (int i = 0; i < elements[hash].Count; i++)
+                    if (elements[hash][i].Equals(item))
+                    {
+                        elements[hash].RemoveAt(i);
+                        if (elements[hash].Count == 0)
+                            elements[hash] = null;
+                        Count--;
+                        return true;
+                    }
+
+            return false;
+        }
+        public List<T> GetElementsBy(int hash)
+        {
+            if (hash > 0 && hash < Size)
+                return elements[hash];
+            else
+                throw new Exception("Неверный хэш");
+        }
+        public List<T> GetSameElements(T item)
+        {
+            return elements[GetHash(item)];
+            HashSet<T> set = new HashSet<T>();
+        }
+        public bool Contains(T item)
+        {
+            int hash = GetHash(item);
+            if (elements[hash] != null)
+            {
+                foreach (T el in elements[GetHash(item)])
+                    if (el.Equals(item))
+                        return true;
+
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public T[] GetElements()
+        {
+            T[] output = new T[Count];
+            int i = 0;
+            foreach(List<T> list in elements)
+                foreach(T el in list)
+                {
+                    output[i] = el;
+                    i++;
+                }
+
+            return output;
+        }
+        public void Clear()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = null;
+            }
+        }
+
+        public double GetLoadFactor()
+        {
+            int loadElNum = 0;
+            foreach(List<T> el in elements)
+                if (el != null)
+                    loadElNum++;
+
+            return Count / (double)loadElNum;
+        }
+        public double GetEffectiveness()
+        {
+            int perfectCount = (int)Math.Round(Count / (double)Size);
+            if (perfectCount == 0)
+                perfectCount = 1;
+            int difference = 0;
+            foreach (List<T> el in elements)
+                if (el != null)
+                    difference += Math.Abs(perfectCount - el.Count);
+
+            return 1 - (double)difference / Count;
+        }
+        public int GetLenghtOfLongestList()
+        {
+            int maxLength = 0;
+            foreach(List<T> el in elements)
+                if (el != null && el.Count > maxLength)
+                    maxLength = el.Count;
+
+            return maxLength;
+        }
+        public int GetLenghtOfShortestList()
+        {
+            int minLength = Count;
+            foreach (List<T> el in elements)
+                if (el == null)
+                    minLength = 0;
+                else if (el.Count < minLength)
+                    minLength = el.Count;
+
+            return minLength;
+        }
+    }
+}
--- Hash2/Program.cs
+++ Hash2/Program.cs
@@ -9,11 +9,11 @@ namespace Hash2
             HashOAM temp = new HashOAM();
 
             Random rnd = new Random();
-            double[] nums = new double[9000];
+            double[] nums = new double[100000];
             for (int i = 0; i < nums.Length; i++)
-                nums[i] = rnd.NextDouble();
+                nums[i] = rnd.NextDouble()*(rnd.Next(9)+1);
 
-            foreach (var item in nums)
+            /*foreach (var item in nums)
                 temp.Insert(item);
 
             int maxLen = 0;
@@ -29,7 +29,17 @@ namespace Hash2
                     currentLen = 0;
                 }
             }
-            Console.WriteLine(maxLen);
+            Console.WriteLine(maxLen);*/
+
+            ChainHashCollection<double> hashCol = new ChainHashCollection<double>(1000);
+
+            foreach (double num in nums)
+                hashCol.Add(num);
+
+            Console.WriteLine("Коэффициент заполнения:   " + hashCol.GetLoadFactor());
+            Console.WriteLine("Процент эффективности:    " + Math.Round(100 * hashCol.GetEffectiveness()) + "%");
+            Console.WriteLine("Длина кратчайшей цепочки: " + hashCol.GetLenghtOfShortestList());
+            Console.WriteLine("Длина длиннейшей цепочки: " + hashCol.GetLenghtOfLongestList());
         }
     }
 }
